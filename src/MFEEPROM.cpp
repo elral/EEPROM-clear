db2248afdd52f86b0a32d5efcd7f8c10a054c64b
elral <3263285+elral@users.noreply.github.com>
2022-05-24 09:37:17 +0200

    eeprom functions to class

--- src/MFEEPROM.cpp
+++ src/MFEEPROM.cpp
@@ -1,127 +1,72 @@
+//
+// MFEEPROM.cpp
+//
+// (C) MobiFlight Project 2022
+//
+
 #include <Arduino.h>
 #include "MFEEPROM.h"
 #include <EEPROM.h>
 
-uint16_t eepromLength = 0;
+#define EEPROM_SIZE 4096
+#define WORKING         // uncomment this for not working Pico variant
 
-#if defined(ARDUINO_ARCH_AVR) || defined(CORE_TEENSY) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266)
-
-void eeprom_init(void) {
-#if defined(ARDUINO_ARCH_RP2040)
-    EEPROM.begin(2048);
-#endif
+MFEEPROM::MFEEPROM()
+{
+#if !defined(WORKING) && defined(ARDUINO_ARCH_RP2040)
+    EEPROM.begin(EEPROM_SIZE);
     eepromLength = EEPROM.length();
-    return;
+#endif
+#if !defined(ARDUINO_ARCH_RP2040)
+    eepromLength = EEPROM.length();
+#endif
 }
 
-uint16_t eeprom_length() {
+void MFEEPROM::init(void)
+{
+#if defined(WORKING) && defined(ARDUINO_ARCH_RP2040)
+    EEPROM.begin(EEPROM_SIZE);
+    eepromLength = EEPROM.length();
+#endif
+}
+
+
+uint16_t MFEEPROM::get_length(void)
+{
     return eepromLength;
 }
 
-void eeprom_read_block(uint16_t adr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        EEPROM.get(adr + i,data[i]);
+void MFEEPROM::read_block(uint16_t adr, char data[], uint16_t len)
+{
+    for (uint16_t i = 0; i < len; i++) {
+        data[i] = read_char(adr + i);
     }
 }
 
-void eeprom_write_block (uint16_t adr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        EEPROM.put(adr + i,data[i]);
+void MFEEPROM::write_block(uint16_t adr, char data[], uint16_t len)
+{
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i < len; i++) {
+        EEPROM.put(adr + i, data[i]);
     }
-#if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266)
+#if defined(ARDUINO_ARCH_RP2040)
     EEPROM.commit();
 #endif
 }
 
-char eeprom_read_char(uint16_t adr) {
+char MFEEPROM::read_char(uint16_t adr)
+{
     if (adr >= eepromLength) return 0;
     return EEPROM.read(adr);
 }
 
-void eeprom_write_byte (uint16_t adr, char data) {
+void MFEEPROM::write_byte(uint16_t adr, char data)
+{
     if (adr >= eepromLength) return;
     EEPROM.put(adr, data);
-#if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP8266)
+#if defined(ARDUINO_ARCH_RP2040)
     EEPROM.commit();
 #endif
 }
 
-#elif defined(ARDUINO_ARCH_STM32)
-
-#if defined(DATA_EEPROM_BASE)
-# warning  "STM32 w/o buffered API"
-
-void eeprom_init(void) {
-        EEPROM.init();
-        eepromLength = EEPROM.length();
-}
-
-uint16_t eeprom_length() {
-    return eepromLength;
-}
-
-void eeprom_read_block(uint16_t addr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        data[i] = EEPROM.read(addr + i);
-    }
-}
-
-void eeprom_write_block (uint16_t addr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        EEPROM.write(addr + i,data[i]);
-    }
-}
-
-uint8_t eeprom_read_char(uint16_t adr, char data) {
-    if (adr >= eepromLength) return 0;
-    data = EEPROM.read(adr);
-}
-
-void eeprom_write_byte (uint16_t adr, char data) {
-    if (adr >= eepromLength) return;
-    EEPROM.write(adr, data);
-}
-
-#else
-
-void eeprom_init(void) {
-        eeprom_buffer_fill();
-        eepromLength = EEPROM.length();
-}
-
-uint16_t eeprom_length() {
-    return eepromLength;
-}
-
-void eeprom_read_block(uint16_t addr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        data[i] = eeprom_buffered_read_byte(addr + i);
-    }
-}
-
-void eeprom_write_block (uint16_t addr, char data[], uint16_t len) {
-    if (adr + len > eepromLength) return;
-    for (uint16_t i = 0; i<len; i++) {
-        eeprom_buffered_write_byte(addr + i,data[i]);
-    }
-    eeprom_buffer_flush();
-}
-
-char eeprom_read_char(uint16_t adr) {
-    if (adr >= eepromLength) return 0;
-    return eeprom_buffered_read_byte(adr);
-}
-
-void eeprom_write_byte (uint16_t adr, char data) {
-    if (adr >= eepromLength) return;
-    EEPROM.write(adr, data);
-    eeprom_buffer_flush();
-}
-#endif
-
-#endif
+// MFEEPROM.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 #include <Arduino.h>
 #include "MFEEPROM.h"
 
+#define CLEARBYTE 0x0F;
+
+MFEEPROM MFeeprom;
+
 void setup() {
-  
-  eeprom_init();
-  uint16_t bufferlength = eeprom_length();
+
+  MFeeprom.init();
+
+  uint16_t bufferlength = MFeeprom.get_length();
   char buffer[bufferlength];
 
   // initialize the LED pin as an output.
@@ -13,24 +18,23 @@ void setup() {
 
   Serial.begin(115200);
   while (!Serial) delay(10);
-
   
-  Serial.print("Anzahl Zellen: "); Serial.println(eeprom_length());
+  Serial.print("Number of Bytes: "); Serial.println(bufferlength);
   for (int i = 0 ; i < bufferlength ; i++) {
-    buffer[i] = 0xFF;
+    buffer[i] = CLEARBYTE;
   }
 
-  eeprom_write_block(0,buffer, bufferlength);
+  MFeeprom.write_block(0,buffer, bufferlength);   // uncomment this for only reading eeprom content
 
 // turn the LED on when we're done
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Speicher gelöscht");
+  Serial.println("Memory erased");
 
-  Serial.print("Anzahl Zellen: "); Serial.println(bufferlength);
+  Serial.print("Number of Bytes: "); Serial.println(bufferlength);
   for (int i = 0 ; i < bufferlength ; i+=32) {
     Serial.print(i); Serial.print(" to "); Serial.print(i+32); Serial.print(": ");Serial.print("\t");
     for (uint16_t j = i; j < i+32; j++) {
-      Serial.print(eeprom_read_char(j));
+      Serial.print(MFeeprom.read_char(j));
     }
     Serial.println("");
   }
@@ -38,7 +42,7 @@ void setup() {
   for (int i = 0 ; i < bufferlength ; i+=32) {
     Serial.print(i); Serial.print(" to "); Serial.print(i+32); Serial.print(": ");Serial.print("\t");
     for (uint16_t j = i; j<i+32; j++) {
-      uint8_t n = eeprom_read_char(j);
+      uint8_t n = MFeeprom.read_char(j);
       Serial.print("0x");
       Serial.print(n < 16 ? "0" : "");
       Serial.print(n, HEX);
