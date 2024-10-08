420a4ade3ddcec672840e8e66b41361c5f8e3599
elral <3263285+elral@users.noreply.github.com>
2024-10-08 16:46:31 +0200

    add compile flag for only dump

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include "MFEEPROM.h"
 
 #define CLEARBYTE 0xFF;
+#ifdef ARDUINO_AVR_PROMICRO16
+#define TXLED0 PORTD &= ~(1 << 5)
+#define TXLED1 PORTD |= (1 << 5)
+#define RXLED0 PORTB &= ~(1 << 0)
+#define RXLED1 PORTB |= (1 << 0)
+#define TX_RX_LED_INIT DDRD |= (1 << 5), DDRB |= (1 << 0), TXLED0, RXLED0
+#endif
 
 MFEEPROM MFeeprom;
 
@@ -17,25 +24,27 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
-#ifdef PRINTOUT
+#if defined(PRINTOUT) || defined(ONLY_DUMP)
   while (!Serial) delay(10);
   Serial.print("Number of Bytes: "); Serial.println(bufferlength);
 #endif
 
+#if !defined(ONLY_DUMP)
   for (uint16_t i = 0 ; i < bufferlength ; i++) {
     buffer[i] = CLEARBYTE;
   }
 
-  if (MFeeprom.write_block(0,buffer, bufferlength)) {
-    
-#ifdef PRINTOUT
+  if (MFeeprom.write_block(0,buffer, bufferlength)) 
+#endif
+  {
+#if defined(PRINTOUT) && !defined(ONLY_DUMP)
     Serial.println("Memory erased");
   } else {
     Serial.println("Failure! Memory not erased (completly)");
 #endif
   }
 
-#ifdef PRINTOUT
+#if defined(PRINTOUT) || defined(ONLY_DUMP)
   Serial.print("Number of Bytes: "); Serial.println(bufferlength);
   for (uint16_t i = 0 ; i < bufferlength ; i+=32) {
     Serial.print(i); Serial.print(" to "); Serial.print(i+32); Serial.print(": ");Serial.print("\t");
@@ -61,8 +70,17 @@ void setup() {
 
 
 void loop() {
+#ifdef ARDUINO_AVR_PROMICRO16
+  TXLED0;
+  RXLED0;
+  delay(1000);
+  TXLED1;
+  RXLED1;
+  delay(1000);
+#else
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+#endif
 }
